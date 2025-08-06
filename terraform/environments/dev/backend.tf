67df4faab4c80943e2c9951e767c95d41f87a505
Praveena <praveena.y@algorims.com>
2025-08-07 00:15:18 +0530

    Virtual Network

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "tfstate-rg"      
     storage_account_name = "tfstatestorageacct11"      
     container_name       = "tfstate"                 
-    key                  = "VNET.tfstate"  
+    key                  = "VNET1.tfstate"  
   }
 }
 
